22eeb65f22d30134bfa85ae18d1df50ed43483f8
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-09-10 19:04:32 +0100

    added new endpoints

--- AssignmentTwo/main.py
+++ AssignmentTwo/main.py
@@ -4,19 +4,34 @@
 #
 # string slicing is needed
 
+import requests
+
+endpoint = "https://opentdb.com/api.php?amount=50"
+response = requests.get(endpoint)
+print("response is:", response)
+data = response.json()
+#print(data)
+
+
 #create a welcome message from the Knowledge Tester console app.
 print("Welcome! I'm your friendly Knowledge Tester")
 print("Let's begin today by choosing a level!")
 
 #create a variable with the input for user to enter level of difficulty for the quiz
-level = input("Would you like your quiz to be easy, medium or difficult?")
-#create conditionals for feedback depending on the user input.
+level = input("Would you like your quiz to be easy, medium or hard?")
+#create conditionals for feedback depending on the user input and use query string parameters to choose set of questions.
 if level == "easy":
+    endpoint = "https://opentdb.com/api.php?amount=20&difficulty=easy"
     print("OK great, let's keep it easy for today!")
 elif level == "medium":
+    endpoint = "https://opentdb.com/api.php?amount=20&difficulty=medium"
     print("Sounds good. Let's spice things up a bit!")
-else:
+elif level == "hard":
+    endpoint = "https://opentdb.com/api.php?amount=20&difficulty=hard"
     print("I like your style, you must be a knowledge wizz!")
-
+else:
+    print("You need to choose a level!")
 # get level input from user and create a new function that selects all questions with requested level.
 
+for question in data["results"]:
+    print(question["question"])
