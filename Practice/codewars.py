8d5a4d3b6a9bc2f03e601480e92b8b05f5f8881e
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-09-27 12:16:43 +0100

    creating the sql file

--- Practice/codewars.py
+++ Practice/codewars.py
@@ -1,19 +1,17 @@
-# There was a test in your class and you passed it. Congratulations!
-#
-# But you're an ambitious person. You want to know if you're better than the average student in your class.
-#
-# You receive an array with your peers' test scores. Now calculate the average and compare your score!
-#
-# Return true if you're better, else false!
-#
-# Note:
-# Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+# create a function in python that can take a list of numbers squares each number passed into it and then sums the results together.
+from operator import truediv
 
-def square_sum(*number):
-    ttl = 0
-    for num in number:
-        ttl += num**0.5
-    return int(ttl)
 
-print(square_sum(1,2,2))
+# def square_sum(numbers):
+#     return sum(x ** 2 for x in numbers)
+#
+# numbers = [1, 2, 2]
+# result = square_sum(numbers)
+# print (result)
+
+def greet(name, owner):
+    if name = owner:
+        print(f'hello boss')
+    else:
+        print('Hello guest')
 
