43670975c55c8ae33f231e190005875c3df0a185
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-10-15 09:58:10 +0100

    restructuring files

--- AssignmentFour/src/db_utils.py
+++ AssignmentFour/src/db_utils.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from config import USER, PASSWORD, HOST, DATABASE
+from AssignmentFour.src.config import USER, PASSWORD, HOST, DATABASE
 
 class DbConnectionError(Exception):
     pass
